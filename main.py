2535d18b9f199d7faf2ee1b99c4cc48e369e2c95
Chatura Yapa <chatura.yapa@gmail.com>
2020-06-08 12:50:59 +1000

    Renamed scrubber.py as mainpy and moved one level up

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import os
 import time
+import logging
+import configparser
 
-def cleanup (number_of_days,path):
+def cleanup (number_of_days,root_path):
 
         time_in_secs = time.time()-(number_of_days * 24 * 60 * 60)
 
-        for root, dirs, files in os.walk(path, topdown=False):
+        for root, dirs, files in os.walk(root_path, topdown=False):
                 for file_ in files:
                         full_path = os.path.join(root, file_)
                         stat = os.stat(full_path)
@@ -13,8 +15,12 @@ def cleanup (number_of_days,path):
                                 os.remove(full_path)
                                 print ("Removed file " +  file_ +" sucessfully")
 
+def logger(file_name,message):    
+        logging.basicConfig(filename=file_name,filemode='a',format='%(asctime)s,%(name)s %(levelname)s %(message)s',level=logging.DEBUG)
+        logging.info(message)
 
 
 
-        
-        
+if __name__ == '__main__':
+
+        logger("test.log","This is a a test")
