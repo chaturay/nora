6f7713a5c139bedcd0d93c36f160695b3f4e5408
Chatura Yapa <chatura.yapa@gmail.com>
2020-06-09 20:43:51 +1000

    Added function backup

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import configparser
 import pysftp
 
 FORMATTER = logging.Formatter("%(asctime)s:%(levelname)s:%(funcName)s  — %(message)s")
+
 LOG_FILE_PATH="logs/"
 MAIN_LOG_NAME="main.log"
 MAIN_LOG_SIZE=1024*1024
 MAIN_LOG_FILES=10
 
+PRIVATE_KEY_PATH="C:\ProgramData\ssh\ssh_host_rsa_key"
+
+SBC_LIST={"m-cba-nsw-bellavista01":"10.10.1.13","m-cba-nsw-bellavista02":"10.10.1.13","m-cba-nsw-bellavista03":"10.10.1.13"}
+    
 def setup_logger(name, log_file,file_size,file_count,level=logging.INFO):
 
     file_handler = RotatingFileHandler(log_file,maxBytes=file_size,backupCount=file_count)        
@@ -45,10 +50,32 @@ def cleanup (number_of_days,root_path):
             logger.info("Sucessfully deleted %s file(s)",count)
             logger.info("Log file cleanup finished.")
 
-def backup ()
+def backup():
 
+    logger=logging.getLogger("MAIN")
+    logger.info("Starting SBC backups ")
+    count=0
 
-def reports()
+    try:
+        for (device_name,ip_address) in SBC_LIST.items(): 
+            sftp=pysftp.Connection(ip_address,
+            username='sftp',private_key=PRIVATE_KEY_PATH)
+            sftp.get('/code/gzConfig/dataDoc.gz','K:\\testbkup')  # get a remote
+            
+            #os.rename('dataDoc.gz',device_name)
+            sftp.close()
+            
+    except:
+         logger.exception("An Exception Occured")
+
+    else:
+        logger.info("Sucessfully backed %s devices",count)
+        logger.info("Backup finished.")
+    finally:
+        sftp.close()
+    
+        
+#def reports():
     
     
 
@@ -60,6 +87,7 @@ def main():
         logger_main.info("Main Script finished")
 
         cleanup(5,"c:\\temp\\")
+        backup()
 
     except:
         logger_main.exception("An Exception Occured")    
