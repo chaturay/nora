20d07c9adb3289e1177f89bf3f5589917e3c9ca6
Chatura Yapa <chatura.yapa@gmail.com>
2020-06-15 16:34:03 +1000

    Added device based reports

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ MAIN_LOG_FILES=10
 PRIVATE_KEY_PATH="C:\ProgramData\ssh\ssh_host_rsa_key"
 
 BACKUP_DEVICE_LIST={"m-cba-nsw-bellavista-sbc01":"10.10.1.13","m-cba-nsw-bellavista-sbc02":"10.10.1.13","m-cba-nsw-bellavista-sbc03":"10.10.1.13"}
+REPORT_DEVICE_LIST=['m-cba-nsw-bellavista-sbc01_m-cba-nsw-bellavista-sbc02','m-cba-nsw-bellavista-sbc03','m-cba-nsw-bellavista-sbc04','m-cba-nsw-burwood-sbc01_m-cba-nsw-burwood-sbc02','m-cba-nsw-burwood-sbc03','m-cba-nsw-burwood-sbc04']
 
 REMOTE_FILE_PATH='/code/gzConfig/dataDoc.gz'
 LOCAL_FILE_PATH='K:\\'
@@ -85,7 +86,7 @@ def backup():
         sftp.close()
     
         
-def reports(begin_datetime,end_datetime,root_path):
+def reports(begin_datetime,end_datetime,root_path,device_name):
 
 
     pattern = '%d/%m/%Y %H:%M:%S'
@@ -107,41 +108,61 @@ def reports(begin_datetime,end_datetime,root_path):
                         count+=1
     except:
          logger.exception("An Exception Occured")
+         
 
     else:
         logger.info("Found %s file(s)",len(file_list))
-
 
     try:
         logger.info("Loading file(s) starting ")
         df=pd.concat((pd.read_csv(file,usecols=['TimeStamp','CPU Utilization','Memory Utilization','Signaling Sessions']) for file in file_list))
         logger.info("Loaded %s file(s) ",len(file_list))
-        
+
+        tic = time.perf_counter()
+        logger.info("processing data... this may take a while")
         df['TimeStamp']=pd.DatetimeIndex(pd.to_datetime(df['TimeStamp'],unit='s')).tz_localize('UTC').tz_convert('Australia/Sydney')
-        df['Date'] = df['TimeStamp'].dt.strftime('%y/%d/%m')
+        df['Date'] = df['TimeStamp'].dt.strftime('%d/%m/%y')
         df=df.groupby(['Date'])[['CPU Utilization','Memory Utilization','Signaling Sessions']].max()
+
+        df.rename(columns = {'CPU Utilization':'CPU Utilization Max', 'Memory Utilization':'Memory Utilization Max','Signaling Sessions':'Signaling Sessions Max'}, inplace = True) 
+        
         
     except:
          logger.exception("An Exception Occured")
 
     else:
-        print(df)
-        logger.info("Loaded %s file(s) ",len(file_list))
-        logger.info("Loading complete")
+        toc = time.perf_counter()
+        logger.info(f"processing data complete. Operation took {toc-tic:0.4f} seconds")
+
+    try:
+        logger.info("Writing report to file - %s",device_name)
+        df.to_csv(device_name)
+            
+    except:
+         logger.exception("An Exception Occured")
+         
+    else:
+        logger.info("Report writing completed")
+
                           
 def main():
 
     try:
         logger_main=setup_logger("MAIN",LOG_FILE_PATH,MAIN_LOG_SIZE,MAIN_LOG_FILES)      
         logger_main.info("Main script started")
-        logger_main.info("Main Script finished")
+        
 
         #cleanup(5,"c:\\temp\\")
         #backup()
-        reports("15/05/2020 00:00:00","16/05/2020 00:15:00","K:\HDR\CY-SBC01\system\\")
+
+        for name in REPORT_DEVICE_LIST:
+            reports("09/06/2020 00:00:00","11/06/2020 23:00:00",f"K:\\TEST\\reports\\\{name}\\",f"K:\TEST\{name}.csv")
 
     except:
-        logger_main.exception("An Exception Occured")    
+        logger_main.exception("An Exception Occured")
+
+    else:
+        logger_main.info("Main Script finished")
 
     finally:
         logger_main.handlers.pop()
